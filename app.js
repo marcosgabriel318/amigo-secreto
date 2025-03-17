4a61a36536bf777e3ca0d475f8a604209f2842e0
marcosgabriel318 <marcosgabriel.mg318@gmail.com>
2025-03-17 17:17:07 -0300

    feat: adiciona as features de atualizar a lista de amigos e adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigos = [];
+
+function atualizaListadeAmigos() {
+  let nameList = document.getElementById("listaAmigos");
+  nameList.innerHTML = "";
+  nomeAmigos.map((name) => {
+    const li = document.createElement("li");
+    li.innerText = name;
+    nameList.appendChild(li);
+  });
+}
+
+function adicionarAmigo() {
+  const nomeAmigo = document.getElementById("amigo");
+  if (nomeAmigo.value != "") {
+    nomeAmigos.push(nomeAmigo.value);
+    if (nomeAmigos.length > 0) {
+      atualizaListadeAmigos();
+    }
+    nomeAmigo.value = "";
+  } else {
+    alert("Por favor, insira um nome válido");
+  }
+}
+
+//nameList.appendChild("li");
